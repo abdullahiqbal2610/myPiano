05e18cc399916e227fdb49abea23ec9cfc2dd3dd
Abdullah Iqbal <abdullahiqbal27122004@gmail.com>
2026-09-13 15:12:39 +0500

    Initial commit: C++ JUCE project scaffold

---
+++ Source/Main.cpp
@@ -0,0 +1,62 @@
+#include <JuceHeader.h>
+
+class VirtualPianoApplication  : public juce::JUCEApplication
+{
+public:
+    VirtualPianoApplication() {}
+
+    const juce::String getApplicationName() override       { return ProjectInfo::projectName; }
+    const juce::String getApplicationVersion() override    { return ProjectInfo::versionString; }
+    bool moreThanOneInstanceAllowed() override             { return true; }
+
+    void initialise (const juce::String& commandLine) override
+    {
+        mainWindow.reset (new MainWindow (getApplicationName()));
+    }
+
+    void shutdown() override
+    {
+        mainWindow = nullptr;
+    }
+
+    void systemRequestedQuit() override
+    {
+        quit();
+    }
+
+    void anotherInstanceStarted (const juce::String& commandLine) override
+    {
+    }
+
+    class MainWindow    : public juce::DocumentWindow
+    {
+    public:
+        MainWindow (juce::String name)
+            : DocumentWindow (name,
+                              juce::Desktop::getInstance().getDefaultLookAndFeel()
+                                                          .findColour (juce::ResizableWindow::backgroundColourId),
+                              DocumentWindow::allButtons)
+        {
+            setUsingNativeTitleBar (true);
+            
+            auto* comp = new juce::Component();
+            comp->setSize (800, 600);
+            setContentOwned (comp, true);
+
+            centreWithSize (getWidth(), getHeight());
+            setVisible (true);
+        }
+
+        void closeButtonPressed() override
+        {
+            JUCEApplication::getInstance()->systemRequestedQuit();
+        }
+    private:
+        JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainWindow)
+    };
+
+private:
+    std::unique_ptr<MainWindow> mainWindow;
+};
+
+START_JUCE_APPLICATION (VirtualPianoApplication)
